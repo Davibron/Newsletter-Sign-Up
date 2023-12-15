7d10b1b28388b61fc8b5d2a7c968fd94f8a9a5c0
David Omonehin <david.omonehin@gmail.com>
2023-12-15 13:03:23 +0100

    media query, adding of desktop screen

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ function isValidEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
+
+function adjustImageVisibility() {
+  const mobileImage = document.getElementById("mobileImage");
+  const desktopImage = document.getElementById("desktopImage");
+
+  if (window.matchMedia('(max-width: 1023px )').matches) {
+    mobileImage.style.display = "block";
+    desktopImage.style.display = "none";
+  } else {
+    mobileImage.style.display = "none";
+    desktopImage.style.display = "block";
+  }
+}
+
+window.addEventListener('resize', adjustImageVisibility);
+
+adjustImageVisibility()
+

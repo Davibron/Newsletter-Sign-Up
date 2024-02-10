85e20769fe6f0ab10173c76616e0627a8573546e
David Omonehin <david.omonehin@gmail.com>
2024-02-10 18:49:08 +0100

    correction

--- main.js
+++ main.js
@@ -9,9 +9,7 @@ function checkInput() {
   } else {
     input.classList.remove("error");
     errorMessage.style.display = "none";
-  }
 
-  if (input.value.trim() !== "" && isValidEmail(input.value)) {
     main.innerHTML = `<div class='successState'>
       <svg id='successImg' xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>
       <h1>Thanks for subscribing!</h1>
@@ -20,11 +18,12 @@ function checkInput() {
       </p>
       <button onclick='dismiss()'>Dismiss message</button>
     </div>`;
+    document.body.classList.add('successState');
   }
-  document.body.classList.add('successState');
 }
 
 function dismiss() {
+  return main;
 }
 
 function isValidEmail(email) {
